de3a89e9cf6178c2e801355d630a46fff8791fcc
Sergei Orlov <sergei@khanacademy.org>
2015-07-26 16:24:38 -0700

    More scales -- major, jazz_minor

--- bs.py
+++ bs.py
@@ -2,21 +2,33 @@ import random
 
 
 notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+
+major = [0, 2, 4, 5, 7, 9, 11, 0]
 blues = [0, 3, 5, 6, 7, 10, 0]
-gypsy = [0, 1, 3, 6, 7, 8, 11, 0]
+hungarian_minor = [0, 2, 3, 6, 7, 8, 11, 0]
+jazz_minor = [0, 2, 3, 5, 7, 9, 11, 0]
 
 
 def make_scale(starting_note, scale_indexes):
+    """Get string of notes representing the desired scale.
+
+    starting_note: A string note.  Examples: 'C#', 'F'.  Must appear in the
+        global `notes` array.
+    scale_indexes: List of indexes that make up the scale.  Can also be thought
+        of as the distance from the starting note of the scale, in half-tones.
+    """
     start_index = notes.index(starting_note)
     all_notes = list(notes)
-    all_notes = notes[start_index:] + notes[:start_index]
+    all_notes = all_notes[start_index:] + all_notes[:start_index]
     return '  '.join(all_notes[i] for i in scale_indexes)
 
 
 def main():
     start = random.choice(notes)
-    print make_scale(start, blues)
-    print make_scale(start, gypsy)
+    for type_ in ['major', 'blues', 'hungarian_minor', 'jazz_minor']:
+        scale_indexes = globals()[type_]
+        print type_ + ':'
+        print make_scale(start, scale_indexes)
 
 
 if __name__ == '__main__':
