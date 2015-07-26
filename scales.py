17d30da326c7988c4a87f8d79f9dc623a09e30ad
Sergei Orlov <sergei@khanacademy.org>
2015-07-26 16:36:32 -0700

    Let's get all unicode up in here

--- scales.py
+++ scales.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 import random
 
 
-notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+notes = ['C', 'C♯', 'D', 'E♭', 'E', 'F', 'F♯', 'G', 'A♭', 'A', 'B♭', 'B']
 
 major = [0, 2, 4, 5, 7, 9, 11, 0]
 blues = [0, 3, 5, 6, 7, 10, 0]
@@ -12,7 +13,7 @@ jazz_minor = [0, 2, 3, 5, 7, 9, 11, 0]
 def make_scale(starting_note, scale_indexes):
     """Get string of notes representing the desired scale.
 
-    starting_note: A string note.  Examples: 'C#', 'F'.  Must appear in the
+    starting_note: A string note.  Examples: 'C♯', 'F'.  Must appear in the
         global `notes` array.
     scale_indexes: List of indexes that make up the scale.  Can also be thought
         of as the distance from the starting note of the scale, in half-tones.
